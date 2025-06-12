acea6241ce72d7ba0d5ff64e3b7d91d50330b2ad
Ramy Ramadan <ramy@isnaad.sa>
2025-06-12 19:54:57 +0300

    init promo-code-task

---
+++ app/Http/Controllers/PromoCode.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\GeneratePromoCodeRequest;
+use App\Http\Requests\CheckPromoCodeRequest;
+
+class PromoCode extends Controller
+{
+ 
+    public function generate(GeneratePromoCodeRequest $request) {
+        
+    }
+    
+    public function check($param) {
+        
+    }
+    
+    private function validate($param) {
+        
+    }
+}
---
+++ app/Http/Services/PromoCodeService.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Requests\Services;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PromoCodeService
+{
+
+}
